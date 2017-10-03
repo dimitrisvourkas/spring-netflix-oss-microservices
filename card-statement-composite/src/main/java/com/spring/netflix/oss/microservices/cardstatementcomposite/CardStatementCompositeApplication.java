e68fbfb0ec684f04129cae43f9bb0c05489b99af
Dimitrios Vourkas <dimitris.vourkas@gmail.com>
2017-10-03 21:01:22 +0200

    Complete

--- card-statement-composite/src/main/java/com/spring/netflix/oss/microservices/cardstatementcomposite/CardStatementCompositeApplication.java
+++ card-statement-composite/src/main/java/com/spring/netflix/oss/microservices/cardstatementcomposite/CardStatementCompositeApplication.java
@@ -2,8 +2,12 @@ package com.spring.netflix.oss.microservices.cardstatementcomposite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class CardStatementCompositeApplication {
 
 	public static void main(String[] args) {
